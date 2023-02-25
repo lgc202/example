5648aa0688fcb13f4bb0a2f79b88a4a227be88cc
root <root@liguangcai.localdomain>
2023-02-25 15:21:16 +0800

    add utest

--- golang/utest/001base/add.go
+++ golang/utest/001base/add.go
@@ -1,1 +1,5 @@
 package _01base
+
+func add(a, b int) int {
+	return a + b
+}
--- golang/utest/001base/add_test.go
+++ golang/utest/001base/add_test.go
@@ -1,1 +1,16 @@
 package _01base
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	want := 3
+	got := add(1, 2)
+	if got != want {
+		t.Errorf("expect: %d, got: %d", want, got)
+	}
+}
+
+// go test 的选项
+// -v，显示所有测试函数的运行细节
+// -run < regexp>，指定要执行的测试函数
+// -count N，指定执行测试函数的次数
